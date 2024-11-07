e4fd54e85106f5ddd77f3e8d2192bb494b65279e
Ahmad <nazzala8@gmail.com>
2024-11-08 00:50:39 +0200

    Quiz section done

--- app/page.tsx
+++ app/page.tsx
@@ -7,6 +7,7 @@ import ContactUs from "../sections/home/ContactUs/ContactUs";
 import ReviewList from "../sections/home/ReviewSection/ReviewList";
 import Footer from "../sections/Footer/Footer";
 import Hero from "../sections/home/Hero/Hero";
+import Quiz from "../sections/home/Quiz/Quiz";
 
 export default function Home() {
   const startDate = "2024-10-20T10:00:00";
@@ -23,6 +24,7 @@ export default function Home() {
           />
         </Box>
         <Hero></Hero>
+        <Quiz></Quiz>
         <AboutUs></AboutUs>
         <ContactUs></ContactUs>
         <ReviewList></ReviewList>
