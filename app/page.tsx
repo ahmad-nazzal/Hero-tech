0ea69b5fbd70fe15d5d42735cac8f3ab80aca87e
marahsaadeh <marah4saadeh@gmail.com>
2024-11-09 15:26:16 +0200

    Updated image slider, added "Login", "Signup", "Buy", and "Read More" buttons, enhanced responsiveness for all viewport sizes

--- app/page.tsx
+++ app/page.tsx
@@ -8,21 +8,24 @@ import ReviewList from "../sections/home/ReviewSection/ReviewList";
 import Footer from "../sections/Footer/Footer";
 import Hero from "../sections/home/Hero/Hero";
 import Quiz from "../sections/home/Quiz/Quiz";
+import Header from "../sections/header/header";
 
 export default function Home() {
   const startDate = "2024-10-20T10:00:00";
-  const endDate = "2024-10-25T23:59:59";
+  const endDate = "2024-11-25T23:59:59";
 
   return (
     <>
       <ChakraProvider theme={theme}>
-        <Box dir={"rtl"}>
+        
+        <Box dir={"rtl"} >
           <DiscountBanner
             startDate={startDate}
             endDate={endDate}
             promotionMessage="خصومات بنسبة 20% على الكورسات"
           />
         </Box>
+        <Header />
         <Hero></Hero>
         <Quiz></Quiz>
         <AboutUs></AboutUs>
