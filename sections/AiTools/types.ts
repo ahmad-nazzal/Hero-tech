728f34bbe261fa5f0798d0cefc605883cb38ebed
marahsaadeh <marah4saadeh@gmail.com>
2024-12-21 09:31:57 +0200

    Implement user favorites management in AiToolsList
    - Added functionality to mark AI tools as favorites and dynamically filter them based on user preferences.
    - Implemented API endpoints using Next.js for managing user data:
      - GET endpoint to fetch user favorites based on email.
      - POST endpoint to update user favorites in the fakeDB.

--- sections/AiTools/types.ts
+++ sections/AiTools/types.ts
@@ -1,7 +1,17 @@
 export interface AiToolsCardProps {
-   tool_id?: string;
+   tool_id?: number;
    title:string;
    imageURL: string;
    description: string;
    tags: string;
+}
+// types.ts
+export interface User {
+  email: string;
+  password: string;
+  firstName: string;
+  lastName: string;
+  username: string;
+  country: string;
+  favorites?: number[];
 }
