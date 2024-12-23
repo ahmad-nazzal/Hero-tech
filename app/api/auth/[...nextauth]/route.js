c731188e17ace52773e2a47793cfb27d83e139b7
Ahmad <nazzala8@gmail.com>
2024-12-23 19:29:33 +0200

    Convert the db.json to be in the public to Fix deployment issue on vercel

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -1,8 +1,8 @@
 import NextAuth from "next-auth";
 import CredentialsProvider from "next-auth/providers/credentials";
-import fs from 'fs';
-import path from 'path'; 
-import bcrypt from 'bcryptjs'; 
+// import fs from "fs";
+// import path from "path";
+import bcrypt from "bcryptjs";
 
 export const authOptions = {
   providers: [
@@ -12,33 +12,38 @@ export const authOptions = {
       async authorize(credentials) {
         const { email, password } = credentials;
 
-        const filePath = path.join(process.cwd(), 'fakeDB', 'db.json');
-        const data = JSON.parse(fs.readFileSync(filePath, 'utf8'));
+        // const filePath = path.join(process.cwd(), "fakeDB", "db.json");
+        // const data = JSON.parse(fs.readFileSync(filePath, "utf8"));
 
+        const response = await fetch(
+          `${process.env.NEXT_PUBLIC_BASE_URL}/fakeDB/db.json`
+        );
+        if (!response.ok) {
+          throw new Error("Failed to fetch the database.");
+        }
+
+        const data = await response.json();
         const user = data.users.find((user) => user.email === email);
 
         if (!user) {
           console.log("المستخدم غير موجود.");
-          return null; 
+          return null;
         }
-
 
         const passwordMatches = await bcrypt.compare(password, user.password);
 
         if (!passwordMatches) {
-          return null; 
+          return null;
         }
 
         return {
-          id: user.email, 
+          id: user.email,
           name: `${user.firstName} ${user.lastName}`,
           email: user.email,
         };
       },
     }),
   ],
-
-
 
   secret: process.env.NEXTAUTH_SECRET,
   pages: {
